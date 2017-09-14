e7e77928d014e80e78b3d01fad07475fe686ce45
Min Zhao <sebzhaomin@gmail.com>
2017-09-14 23:47:11 +0200

    Work on group chat tests

--- XmppXamarinTest/XmppXamarinTest/XmppXamarinTest.Android/MainActivity.cs
+++ XmppXamarinTest/XmppXamarinTest/XmppXamarinTest.Android/MainActivity.cs
@@ -6,13 +6,18 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Sharp.Xmpp;
 using Sharp.Xmpp.Client;
+using Sharp.Xmpp.Im;
 
 namespace XmppXamarinTest.Droid
 {
     [Activity(Label = "XmppXamarinTest", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static XmppClient _client = new XmppClient("xmpp.jp", "team8test2", "team8proj");
+        Jid _room = new Jid("conference.xmpp.jp", "team8room2");
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -23,14 +28,48 @@ namespace XmppXamarinTest.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
 
-            using (var client = new XmppClient("xmpp.jp", "team8test", "team8ladivine"))
+            //SharedXmppLib.Test.Run();
+
+            //return;
+
+            _client.Hostname = "xmpp.jp";
+            _client.Message += Client_Message;
+            _client.RosterUpdated += _client_RosterUpdated;
+            _client.ActivityChanged += _client_ActivityChanged;
+            _client.GroupInviteReceived += _client_GroupInviteReceived;
+            _client.GroupPresenceChanged += _client_GroupPresenceChanged;
+            _client.Connect();
+            var roster = _client.GetRoster();
+            _client.JoinRoom(_room, "xamarin.android.tentative");
+        }
+
+        private void _client_GroupPresenceChanged(object sender, Sharp.Xmpp.Extensions.GroupPresenceEventArgs e)
+        {
+        }
+
+        private void _client_GroupInviteReceived(object sender, Sharp.Xmpp.Extensions.GroupInviteEventArgs e)
+        {
+            _client.JoinRoom(e.ChatRoom, "xamarin.android");
+        }
+
+        private void _client_ActivityChanged(object sender, Sharp.Xmpp.Extensions.ActivityChangedEventArgs e)
+        {
+        }
+
+        private void _client_RosterUpdated(object sender, Sharp.Xmpp.Im.RosterUpdatedEventArgs e)
+        {
+        }
+
+        private void Client_Message(object sender, Sharp.Xmpp.Im.MessageEventArgs e)
+        {
+            var test = e.Jid.GetBareJid();
+            if (test.ToString() == _room.ToString())
             {
-                client.Hostname = "xmpp.jp";
-                client.Connect();
-                Console.WriteLine("Connected as " + client.Jid);
-                Console.WriteLine(" Type 'send <JID> <Message>' to send a chat message, or 'quit' to exit.");
-                Console.WriteLine(" Example: send user@domain.com Hello!");
-                Console.WriteLine();
+                _client.SendMessage(test, $"feedback @ {DateTime.Now}", type:MessageType.Groupchat);
+            }
+            else
+            {
+                _client.SendMessage(test, $"feedback @ {DateTime.Now}");
             }
         }
     }
